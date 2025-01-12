71373381a054cae3e8484af7861e563a1b7ae251
Jayprajapati19 <jayp37679@gmail.com>
2025-01-12 11:52:32 +0530

    refactor Home component styling for improved typography and button design

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -7,8 +7,8 @@ function Home() {
             <div className='bg-cover  bg-center bg-[url(https://cdn.cars24.com/prod/auto-news24-cms/CARS24-Blog-Images/2024/09/26/dde48427-4b52-46d0-81c2-91df594db64c-RED-LIGHT.jpg)] h-screen pt-8 flex justify-between flex-col w-full '>
                 <img className='w-16 ml-8 ' src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png" alt="logo" />
                 <div className='bg-white pb-7 py-4 px-4'>
-                    <h2 className='text-3xl font-bold '>Get Started with Uber</h2>
-                    <Link to='/login' className='flex items-center justify-center w-full bg-black text-white py-3 rounded mt-5'>Continue</Link>
+                    <h2 className='text-[30px] font-semibold '>Get Started with Uber</h2>
+                    <Link to='/login' className='flex items-center justify-center w-full bg-black text-white py-3 rounded-lg mt-5'>Continue</Link>
 
                 </div>
             </div>
