c95dfd9bf72887985e2d57ce6cc58ebefed77019
Jayprajapati19 <jayp37679@gmail.com>
2025-01-16 07:03:54 +0530

    enhance CaptainHome layout with improved header, driver details, and earnings display

--- frontend/src/pages/CaptainHome.jsx
+++ frontend/src/pages/CaptainHome.jsx
@@ -1,53 +1,55 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
 
 function CaptainHome() {
     return (
         <div className='h-screen '>
-            <Link to='/home' className='fixed  right-2 top-2 h-10 w-10 bg-white flex items-center justify-center rounded-full '>
-                <i className="text-lg font-medium ri-home-5-line"></i>
+            <div className='fixed p-6 top-0 flex items-center justify-between w-screen '>
+                <img className='w-16 ' src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png" alt="" />
+                <Link to='/captain-home' className=' h-10 w-10 bg-white flex items-center justify-center rounded-full '>
+                    <i className="text-lg font-medium ri-logout-box-r-line"></i>
 
-            </Link>
+                </Link>
+            </div>
 
-            <div className='h-1/2 '>
+            <div className='h-3/5 '>
                 <img className='h-full w-full object-cover ' src="https://www.hanbit.co.kr/data/editor/20210429161116_qvzgnfvw.gif" alt="map" />
             </div>
 
-            <div className='h-1/2 p-4'>
+            <div className='h-2/5 p-6 '>
 
-                <div className='flex items-center justify-between'>
-                    <img className='h-12' src="https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_552,w_552/v1555367310/assets/30/51e602-10bb-4e65-b122-e394d80a9c47/original/Final_UberX.png" alt="car" />
-                    <div className='text-right '>
-                        <h2 className='text-lg font-medium '>Jay Prajapati </h2>
-                        <h4 className='text-xl font-semibold -mt-1 '>GJ 02 JD 1909</h4>
-                        <p className='text-sm text-gray-600 '>Maruti Suzuki Alto</p>
-
+                <div className='flex items-center justify-between '>
+                    <div className='flex items-center justify-start gap-3'>
+                        <img className='h-10 w-10 rounded-full object-cover ' src="https://plus.unsplash.com/premium_photo-1689530775582-83b8abdb5020?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cmFuZG9tJTIwcGVyc29ufGVufDB8fDB8fHww" alt="" />
+                        <h4 className='text-lg font-medium '>Jay </h4>
                     </div>
 
-                </div>
-
-                <div className='flex gap-2 justify-between flex-col items-center'>
-                    <div className='w-full mt-5'>
-
-                        <div className='flex items-center gap-5 p-3 border-b-2 '>
-                            <i className="text-lg ri-map-pin-2-fill"></i>
-
-                            <div>
-                                <h3 className='text-lg font-medium '>562/11-A</h3>
-                                <p className='text-sm -mt-1 text-gray-600'>Kankariya Talab, Ahemdabad</p>
-                            </div>
-                        </div>
-                        <div className='flex items-center gap-5 p-3  '>
-                            <i className="text-lg ri-money-rupee-circle-fill"></i>
-
-                            <div>
-                                <h3 className='text-lg font-medium '>₹119.30</h3>
-                                <p className='text-sm -mt-1 text-gray-600'>Cash Cash</p>
-                            </div>
-                        </div>
+                    <div>
+                        <h4 className='text-xl font-semibold '>₹255.20</h4>
+                        <p className='text-sm text-gray-600'>Earned</p>
                     </div>
                 </div>
 
-                <button className='w-full mt-5 bg-green-600 text-white font-semibold p-2 rounded-lg '>Make a Payment</button>
+                <div className='flex p-3 mt-8 bg-gray-100 rounded-xl  justify-center gap-5 items-start'>
+                    <div className='text-center '>
+                        <i className="text-3xl font-thin   ri-timer-2-line"></i>
+                        <h5 className='text-lg mb-2  font-medium'>10.2</h5>
+                        <p className='text-sm text-gray-600  '>Hours Online</p>
+                    </div>
+                    <div className='text-center '>
+                        <i className="text-3xl font-thin   ri-speed-up-line"></i>
+                        <h5 className='text-lg mb-2  font-medium'>10.2</h5>
+                        <p className='text-sm text-gray-600  '>Hours Online</p>
+                    </div>
+                    <div className='text-center '>
+                        <i className="text-3xl font-thin   ri-booklet-line"></i>
+                        <h5 className='text-lg mb-2  font-medium'>10.2</h5>
+                        <p className='text-sm text-gray-600  '>Hours Online</p>
+                    </div>
+
+
+                </div>
             </div>
 
         </div>
